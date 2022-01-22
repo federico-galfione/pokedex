c0068356b970e5606f0c5c93547cb605222f7577
Federico Galfione <federico.galfione@outlook.com>
2022-01-22 22:56:56 +0100

    Add new components to pokedex module

--- src/app/pokedex/pokedex.module.ts
+++ src/app/pokedex/pokedex.module.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
+import { PokemonTypeComponent } from './components/pokemon-type-icon/pokemon-type.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { PokedexRoutingModule } from './pokedex-routing.module';
 
 
-
 @NgModule({
   declarations: [
-    ListPageComponent
+    ListPageComponent,
+    PokemonItemComponent,
+    PokemonTypeComponent
   ],
   imports: [
     CommonModule,
-    PokedexRoutingModule
+    PokedexRoutingModule,
+    MatIconModule
   ]
 })
 export class PokedexModule { }
