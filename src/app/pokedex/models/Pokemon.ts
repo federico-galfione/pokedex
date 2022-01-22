3b859c79475939102ef8ead7fe90a5c6c96230ca
Federico Galfione <federico.galfione@outlook.com>
2022-01-22 14:47:32 +0100

    Add basic models

---
+++ src/app/pokedex/models/Pokemon.ts
@@ -0,0 +1,81 @@
+import { PokeapiResource } from './PokeapiResource';
+
+/**
+ * Simplified interfaces to manage API data.
+ * https://github.com/monbrey/pokeapi-typescript
+ */
+
+export interface Pokemon {
+    id: number;
+    name: string;
+    base_experience: number;
+    height: number;
+    is_default: boolean;
+    order: number;
+    weight: number;
+    moves: PokemonMoveResource[];
+    sprites: PokemonSprites;
+    stats: PokemonStat[];
+    types: {
+        slot: number,
+        type: PokeapiResource<Type, PokemonTypeName>
+    };
+}
+
+export interface PokemonMoveResource {
+    move: PokeapiResource<Move>;
+}
+
+export interface Move {
+    id: number;
+    name: string;
+    accuracy: number;
+    effect_chance: number;
+    pp: number;
+    priority: number;
+    power: number;
+    generation: PokeapiResource<Generation>;
+}
+
+export interface Generation{
+    id: number;
+    name: string;
+}
+
+export interface Type {
+    id: number;
+    name: string;
+    damage_relations: TypeRelations;
+}
+
+export interface TypeRelations {
+    no_damage_to: Array<PokeapiResource<Type>>;
+    half_damage_to: Array<PokeapiResource<Type>>;
+    double_damage_to: Array<PokeapiResource<Type>>;
+    no_damage_from: Array<PokeapiResource<Type>>;
+    half_damage_from: Array<PokeapiResource<Type>>;
+    double_damage_from: Array<PokeapiResource<Type>>;
+}
+
+export interface PokemonSprites{
+    front_default: string;
+    front_shiny: string;
+    front_female: string;
+    front_shiny_female: string;
+    back_default: string;
+    back_shiny: string;
+    back_female: string;
+    back_shiny_female: string;
+}
+
+export interface PokemonStat {
+    stat: PokeapiResource<Stat>;
+    effort: number;
+    base_stat: number;
+}
+
+export interface Stat {
+    id: number;
+    name: string;
+    name_index: number;
+}
---
+++ src/app/pokedex/models/PokemonType.d.ts
@@ -0,0 +1,1 @@
+type PokemonTypeName = "normal" | "fighting" | "flying" | "poison" | "ground" | "rock" | "bug" | "ghost" | "steel" | "fire" | "water" | "grass" | "electric" | "psychic" | "ice" | "dragon" | "dark" | "fairy"
